6b5158100f630ace51e57db145d64a99e143ae74
ROHAN S PES2UG22CS456 <166039478+RohanS2003CHAINSAWMAN36@users.noreply.github.com>
2025-03-15 18:42:54 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES2UG22CS456 have successfully built and run this  \n";
+  cout << "PES2UG22CS456 have successfully built and ran this again.\n";
   return 0;
 }
